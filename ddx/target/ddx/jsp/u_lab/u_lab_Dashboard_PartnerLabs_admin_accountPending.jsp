6db08bec68a3a53c0697dd40b51fa4c0acb8943a
butterfly-effect <lcging@163.com>
2014-11-25 17:29:17 +0800

    asd

--- ddx/target/ddx/jsp/u_lab/u_lab_Dashboard_PartnerLabs_admin_accountPending.jsp
+++
@@ -1,3 +0,0 @@
-<%@page pageEncoding="UTF-8" contentType="text/html; charset=UTF-8"%>
-<h1>合作账号激活中</h1>
-您申请的 <strong>${requestAccountLab.name} </strong> 技工间合作账号正等待审批,如想了解更多申请状态的细节请直接联系 <strong>${requestAccountLab.name} </strong>技工间。
--- ddx/target/ddx/jsp/u_practice/js/u_practice_index.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	/*$("#addLab_id").click(function(){
-		window.location.href= webContext + "/practiceAction/pageJump.do?type=signUpToPortal";
-	});*/
-	
-	$("#addLabT_id").click(function(){
-		window.location.href= webContext + "/practiceAction/pageJump.do?type=signUpToPortal";
-	});
-});
